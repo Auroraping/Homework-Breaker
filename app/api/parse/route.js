9cba8a94a2e0273788d76b605f59a36fddc0db74
liangy <liang_yue2026@163.com>
2026-06-24 16:21:14 +0800

    Fix DOMMatrix build issue

--- app/api/parse/route.js
+++ app/api/parse/route.js
@@ -1,3 +1,7 @@
+if (typeof global.DOMMatrix === 'undefined') global.DOMMatrix = class DOMMatrix {};
+if (typeof global.ImageData === 'undefined') global.ImageData = class ImageData {};
+if (typeof global.Path2D === 'undefined') global.Path2D = class Path2D {};
+
 import { NextResponse } from 'next/server';
 import mammoth from 'mammoth';
 const pdf = require('pdf-parse');
